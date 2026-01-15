08593bf8e5ccfa4916faeb9c51e4905b12597bec
Dip Narayan <tec27706@adobe.com>
2026-01-15 18:25:30 +0530

    Add heading option

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,35 +1,15 @@
 export default function decorate(block) {
-    const data = block.dataset;
+    const [quoteWrapper] = block.children;
 
-    const quote = data.quote;
-    const author = data.author;
-    const showAsHeading = data.showAsHeading === 'true';
-
-    const allowed = ['h2', 'h3', 'h4', 'h5', 'h6'];
-    const headingType = allowed.includes(data.headingType)
-        ? data.headingType
-        : 'h2';
-
-    // Clear block
-    block.innerHTML = '';
-
-    // Create blockquote wrapper
     const blockquote = document.createElement('blockquote');
+    blockquote.textContent = quoteWrapper.textContent.trim();
 
-    // Create quote text element
-    const textEl = document.createElement(
-        showAsHeading ? headingType : 'p'
-    );
-    textEl.textContent = quote;
+    const data = block.dataset;
+    const allowed = ['h2', 'h3', 'h4', 'h5', 'h6'];
+    const headingType = allowed.includes(data.headingType);;
+    if (headingType) {
 
-    blockquote.append(textEl);
-
-    // Optional author
-    if (author) {
-        const authorEl = document.createElement('cite');
-        authorEl.textContent = author;
-        blockquote.append(authorEl);
     }
-
-    block.append(blockquote);
+    quoteWrapper.replaceChildren(blockquote);
 }
+
