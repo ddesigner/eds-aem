0edcc1fe0ccfc7c341795bd8080ba3298710cc9f
Dip Narayan <tec27706@adobe.com>
2026-01-16 10:31:49 +0530

    update quote js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,23 +1,38 @@
 export default function decorate(block) {
-    const [quoteWrapper] = block.children;
     const [
-        quoteEl,
+        quoteWrapper,
         authorEl,
         showAsHeadingEl,
         headingTypeEl
     ] = block.children;
 
-    const selectedHeading = headingTypeEl?.textContent?.trim() || 'h2';
+    const selectedHeading = headingTypeEl?.textContent?.trim();
+    const showAsHeadingEnable = showAsHeadingEl?.showAsHeadingEl?.trim();
+
     console.log('selectedHeading', selectedHeading);
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
 
-    const data = block.dataset;
     const allowed = ['h2', 'h3', 'h4', 'h5', 'h6'];
-    const headingType = allowed.includes(data.headingType);;
-    if (headingType) {
+    const isValidHeading = showAsHeadingEnable === "true" && selectedHeading && allowed.includes(selectedHeading.toLowerCase());
+    if (isValidHeading) {
+        // Wrap blockquote with selected heading
+        const heading = document.createElement(selectedHeading.toLowerCase());
+        heading.appendChild(blockquote);
 
+        quoteWrapper.replaceWith(heading);
+    } else {
+        // Unwrap heading if it exists and keep blockquote only
+        const parent = quoteWrapper.parentElement;
+
+        if (parent && allowed.includes(parent.tagName.toLowerCase())) {
+            parent.replaceWith(blockquote);
+        } else {
+            quoteWrapper.replaceWith(blockquote);
+        }
     }
-    quoteWrapper.replaceChildren(blockquote);
+
+    // quoteWrapper.replaceChildren(blockquote);
 }
+
 
