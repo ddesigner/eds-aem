02dc8f15524f2026371c3e43fb7693826649c3e8
Dip Narayan <tec27706@adobe.com>
2026-01-16 13:42:17 +0530

    update placeholder

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,4 +1,6 @@
-export default function decorate(block) {
+import { fetchPlaceholders } from '../../scripts/placeholders.js';
+
+export default async function decorate(block) {
     const [
         quoteWrapper,
         authorEl,
@@ -6,12 +8,16 @@ export default function decorate(block) {
         headingTypeEl
     ] = block.children;
 
+
+
     const selectedHeading = headingTypeEl?.textContent?.trim();
     const showAsHeadingEnable = showAsHeadingEl?.textContent?.trim();
 
     console.log('selectedHeading', selectedHeading);
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
+
+
 
     const allowed = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
     const isValidHeading = showAsHeadingEnable === "true" && selectedHeading && allowed.includes(selectedHeading.toLowerCase());
@@ -32,6 +38,14 @@ export default function decorate(block) {
         }
     }
 
+    // Append suffix at the bottom
+    const placeholders = await fetchPlaceholders();
+    const quoteSuffix = placeholders.quoteSuffix;
+    if (quoteSuffix) {
+        const suffixEle = document.createElement('p');
+        suffixEle.textContent = quoteSuffix;
+        blockquote.appendChild(suffixEle);
+    }
     // quoteWrapper.replaceChildren(blockquote);
 }
 
