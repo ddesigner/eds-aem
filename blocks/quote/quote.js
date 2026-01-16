30690b225e63f1a1c931c44690121882a7a2e238
Dip Narayan <tec27706@adobe.com>
2026-01-16 10:44:44 +0530

    update quote js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -13,16 +13,16 @@ export default function decorate(block) {
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
 
-    const allowed = ['h2', 'h3', 'h4', 'h5', 'h6'];
+    const allowed = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
     const isValidHeading = showAsHeadingEnable === "true" && selectedHeading && allowed.includes(selectedHeading.toLowerCase());
     if (isValidHeading) {
         // Wrap blockquote with selected heading
         const heading = document.createElement(selectedHeading.toLowerCase());
-        heading.appendChild(blockquote);
-
-        quoteWrapper.replaceWith(heading);
+        heading.textContent = blockquote.textContent;
+        blockquote.textContent = '';
+        blockquote.appendChild(heading);
+        quoteWrapper.replaceWith(blockquote);
     } else {
-        // Unwrap heading if it exists and keep blockquote only
         const parent = quoteWrapper.parentElement;
 
         if (parent && allowed.includes(parent.tagName.toLowerCase())) {
